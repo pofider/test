bfacdb15a2c151ee7635ceddb35ed7e86f61a00c
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-04 17:40:58 +0100

    add api info message

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var jsreport = require('jsreport')()
 
+var date = new Date().getTime()
+jsreport.on('express-configure', function (app) {
+   app.get('/api/test', function (req, res) {
+     res.send('1 - ' + date)
+   })
+})
+
 jsreport.init(function () {
+
+
   // running
 }).catch(function (e) {
   // error during startup
