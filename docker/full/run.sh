85f4e2334a45d3018322b7d9ecfb4985dc2637b0
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-11 17:51:01 +0100

    a

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -1,5 +1,10 @@
 # file run.sh
 #!/bin/sh
 
-Xvfb :99 -screen 0 1024x768x24 &
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 & export DISPLAY=:99.0 && node "/usr/src/app/index.js"
+echo starting xvfb
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+export DISPLAY=:99.0
+
+echo starting node
+node "/usr/src/app/index.js"
+echo node is now running
