0363fbf78930fe7aef4849319248656675a45560
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-11 16:40:41 +0100

    finally?

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -1,27 +1,5 @@
 # file run.sh
 #!/bin/sh
 
-XVFB=/usr/bin/Xvfb
-XVFBARGS="$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
-PIDFILE=/var/xvfb_${DISPLAY:1}.pid
-case "$1" in
-  start)
-    echo -n "Starting virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
-    echo "."
-    ;;
-  stop)
-    echo -n "Stopping virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
-    echo "."
-    ;;
-  restart)
-    $0 stop
-    $0 start
-    ;;
-  *)
-  echo "Usage: /etc/init.d/xvfb {start|stop|restart}"
-  exit 1
-esac
-
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 & export DISPLAY=:99.0 && node "/usr/src/app/index.js"
+Xvfb :99 -screen 0 1024x768x24 &
