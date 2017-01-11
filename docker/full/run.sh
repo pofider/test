d670552eb8f8659bb2bb377133d7b098b0d77f8d
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-11 18:25:40 +0100

    docker-chromium-xvfb

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -1,5 +1,22 @@
 # file run.sh
 #!/bin/sh
 
-export DISPLAY=:99.0 && node "/usr/src/app/index.js"
+_kill_procs() {
+  kill -TERM $node
+  wait $node
+  kill -TERM $xvfb
+}
 
+# Setup a trap to catch SIGTERM and relay it to child processes
+trap _kill_procs SIGTERM
+
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+xvfb=$!
+
+export DISPLAY=:99
+
+node "/usr/src/app/index.js"
+node=$!
+
+wait $node
+wait $xvfb
