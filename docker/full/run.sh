f69d7f2da904cd16f4e3120eaf7c3b9c87fb670d
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-11 19:52:41 +0100

    madness

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -1,22 +1,5 @@
 # file run.sh
 #!/bin/sh
 
-_kill_procs() {
-  kill -TERM $node
-  wait $node
-  kill -TERM $xvfb
-}
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 & export DISPLAY=:99 && node "/usr/src/app/index.js"
 
-# Setup a trap to catch SIGTERM and relay it to child processes
-trap _kill_procs SIGTERM
-
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-xvfb=$!
-
-export DISPLAY=:99
-
-node "/usr/src/app/index.js"
-node=$!
-
-wait $node
-wait $xvfb
