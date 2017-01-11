c87b196bab570c3820824a44110b4d456705f495
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-11 15:17:52 +0100

    electron version

--- docker/full/index.js
+++ docker/full/index.js
@@ -1,15 +1,18 @@
-﻿var fs = require('fs'),
-    convertFactory = require('electron-html-to'),
+﻿/*var fs = require('fs'),
+    convertFactory = require('electron-html-to'),*/
     http = require('http')
-
+/*
 var conversion = convertFactory({
   converterPath: convertFactory.converters.PDF
-});
+});*/
+
+
+var electron = require('electron-prebuilt');
+var spawn = require('child_process').spawn
 
 
 const server = http.createServer((req, res) => {
-  console.log('request')
-  conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
+  /*conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
     if (err) {
       res.statusCode = 500
       res.setHeader('Content-Type', 'text/plain')
@@ -19,6 +22,31 @@ const server = http.createServer((req, res) => {
 
     console.log(result.numberOfPages);
     result.stream.pipe(res);
+  });*/
+
+  if (req.method !== 'GET' || req.url !== '/' ) {
+    return res.end(0)
+  }
+
+  console.log('request')
+
+
+  var ch = spawn(electron, ['--version'], {
+    env: {
+      DISPLAY: process.env.DISPLAY
+    }
+  })
+
+  var result = ''
+  function data(d) {
+    console.log(d.toString())
+    result += d
+  }
+
+  ch.stdout.on('data', data);
+  ch.stderr.on('data', data);
+  ch.on('close', function () {
+    res.end(result)
   });
 });
 
