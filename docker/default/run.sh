970b25ff55aacf29d44a6d44bb0550de8fc79a7a
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-12 14:10:14 +0100

    user jsreport

--- docker/default/run.sh
+++ docker/default/run.sh
@@ -1,37 +1,36 @@
 # file run.sh
 #!/bin/sh
 
-if [ -d "/jsreport" ]; then 
-
+if [ -d "/jsreport" ]; then
   # link data folder from mounted volume
 
-  if [ ! -d "/jsreport/data" ]; then 
+  if [ ! -d "/jsreport/data" ]; then
     mkdir "/jsreport/data"
   fi
 
-  ln -nsf "/jsreport/data" "/app/data"
+  ln -nsf "/jsreport/data" "/usr/src/app/data"
 
-  # copy default config  
+  # copy default config
 
-  if [ ! -f "/jsreport/prod.config.json" ]; then    
-    cp "/app/prod.config.json" "/jsreport/prod.config.json"    
+  if [ ! -f "/jsreport/prod.config.json" ]; then
+    cp "/usr/src/app/prod.config.json" "/jsreport/prod.config.json"
   fi
 
   if [ ! -f "/jsreport/dev.config.json" ]; then
-      cp "/app/dev.config.json" "/jsreport/dev.config.json"
+      cp "/usr/src/app/dev.config.json" "/jsreport/dev.config.json"
   fi
 
   # delete default config and link from volume
-	
-  rm -f "/app/prod.config.json"
-  ln -s "/jsreport/prod.config.json" "/app/prod.config.json"
 
-  rm -f "/app/dev.config.json"
-  ln -s "/jsreport/dev.config.json" "/app/dev.config.json"
+  rm -f "/usr/src/app/prod.config.json"
+  ln -s "/jsreport/prod.config.json" "/usr/src/app/prod.config.json"
+
+  rm -f "/usr/src/app/dev.config.json"
+  ln -s "/jsreport/dev.config.json" "/usr/src/app/dev.config.json"
 
   chown -R jsreport:jsreport /jsreport
 fi
 
 su jsreport
 
-NODE_ENV=${NODE_ENV:-production} node "/app/server.js"
+node "/usr/src/app/server.js"
