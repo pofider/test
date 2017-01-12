28a05e44cb88b71054cd57ec759cbde18b27b9d5
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-12 11:32:19 +0100

    full jsreport

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -8,4 +8,4 @@ echo Running display 99
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 echo Starting node.js
-export DISPLAY=:99 && node "/usr/src/app/index.js"
+export DISPLAY=:99 && node "/usr/src/app/server.js"
