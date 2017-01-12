d2a239780a45f80652577643ca13834e8c4b592b
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-12 10:17:13 +0100

    getting close

--- docker/full/run.sh
+++ docker/full/run.sh
@@ -1,10 +1,7 @@
 # file run.sh
 #!/bin/sh
 
-export DISPLAY=:99 && node "/usr/src/app/index.js" &
-node=$!
-
+rm /tmp/.X99-lock
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
-wait $node
-
+export DISPLAY=:99 && node "/usr/src/app/index.js"
