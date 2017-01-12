4c88f1beee307e9d871844c84a170d27f6e21aec
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-12 10:55:02 +0100

    use conversion

--- docker/full/index.js
+++ docker/full/index.js
@@ -1,20 +1,13 @@
-﻿/*var fs = require('fs'),
-    convertFactory = require('electron-html-to'),*/
+﻿var fs = require('fs'),
+    convertFactory = require('electron-html-to'),
     http = require('http')
-/*
+
 var conversion = convertFactory({
   converterPath: convertFactory.converters.PDF
-});*/
-
-
-var electron = require('electron-prebuilt');
-var spawn = require('child_process').spawn
-
-// changed
-
+});
 
 const server = http.createServer((req, res) => {
-  /*conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
+  conversion({ html: '<h1>Hello World</h1>' }, function(err, result) {
     if (err) {
       res.statusCode = 500
       res.setHeader('Content-Type', 'text/plain')
@@ -24,31 +17,6 @@ const server = http.createServer((req, res) => {
 
     console.log(result.numberOfPages);
     result.stream.pipe(res);
-  });*/
-
-  if (req.method !== 'GET' || req.url !== '/' ) {
-    return res.end(0)
-  }
-
-  console.log('request')
-
-
-  var ch = spawn(electron, ['--version'], {
-    env: {
-      DISPLAY: process.env.DISPLAY
-    }
-  })
-
-  var result = ''
-  function data(d) {
-    console.log(d.toString())
-    result += d
-  }
-
-  ch.stdout.on('data', data);
-  ch.stderr.on('data', data);
-  ch.on('close', function () {
-    res.end(result)
   });
 });
 
--- docker/full/run.sh
+++ docker/full/run.sh
@@ -1,7 +1,11 @@
 # file run.sh
 #!/bin/sh
 
-rm /tmp/.X99-lock
+echo Trying to remove the lock on displat 99
+rm /tmp/.X99-lock > /dev/null 2>&1
+
+echo Running display 99
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
+echo Starting node.js
 export DISPLAY=:99 && node "/usr/src/app/index.js"
