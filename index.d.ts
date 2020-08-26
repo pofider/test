0f855b5f268efcdad8ec3b8dcf3cb036fcc51970
pofider <honza.pofider@seznam.cz>
2020-08-26 21:51:58 +0200

    a

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,15 @@
 // Type definitions for jsreport-jsrender 1.0
-// Project: https://github.com/jsreport/jsreport-jsrender
-// Definitions by: taoqf <https://github.com/taoqf>
+// Project: https://github.com/jsreport/jsreport
+// Definitions by: pofider <https://github.com/pofider>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
+// import all availible types for jsreport included extensions 
 import JsReportChromePdf = require('jsreport-chrome-pdf')
+import JsReportJsRender = require('jsreport-jsrender')
+import JReportHtmlToXlsx = require('jsreport-html-to-xlsx')
+import JsReportXlsx = require('jsreport-xlsx')
+
+// just reexport types from core
 import JsReport = require('jsreport-core');
 export = JsReport;
