22caface85e056248f996e5591e2d4ca60748674
pofider <honza.pofider@seznam.cz>
2020-08-26 20:04:25 +0200

    test types

--- test.js
+++
@@ -1,6 +0,0 @@
-const puppeteer = require('puppeteer')
-console.log('launch')
-puppeteer.launch({
-  executablePath: 'google-chrome-unstable',
-  args: ['--no-sandbox', '--disable-dev-shm-usage'/*, '--user-data-dir=/mytemp'*/]
-}).then(() => console.log('launched')).catch(console.error)
