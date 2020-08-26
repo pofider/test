a37405dbd8841085dadb03f2526efe10e6a67df8
pofider <honza.pofider@seznam.cz>
2020-08-26 21:33:36 +0200

    test2

--- index.d.ts
+++ index.d.ts
@@ -4,5 +4,6 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.3
 
+import JsReportChromePdf = require('jsreport-chrome-pdf')
 import JsReport = require('jsreport-core');
 export = JsReport;
