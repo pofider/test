b4701ed54dd1b545ba8640870654aa1efa1fa194
pofider <honza.pofider@seznam.cz>
2022-01-12 22:25:57 +0100

    compiles

--- compile.js
+++ compile.js
@@ -1,3 +1,13 @@
 const fs = require('fs')
 
-fs.writeFileSync('jsreport-win.zip', 'hello')
+if (process.platform === 'win32') {
+    fs.writeFileSync('jsreport-win.zip', 'win')
+    return
+}
+
+if (process.platform === 'darwin') {
+    fs.writeFileSync('jsreport-osx.tar.gz', 'osx')
+    return
+}
+
+fs.writeFileSync('jsreport-linux.tar.gz', 'linux')
